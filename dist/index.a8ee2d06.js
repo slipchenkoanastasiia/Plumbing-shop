6481a6c63a3e158b3fa7b96a1e0d462e209dd680
slipchenkoanastasiia <138067087+slipchenkoanastasiia@users.noreply.github.com>
2025-02-03 23:54:44 +0200

    add header

---
+++ dist/index.a8ee2d06.js
@@ -0,0 +1,7 @@
+window.addEventListener('scroll', function() {
+    const header = document.querySelector('header');
+    if (window.scrollY > 50) header.classList.add('scrolled');
+    else header.classList.remove('scrolled');
+});
+
+//# sourceMappingURL=index.a8ee2d06.js.map
