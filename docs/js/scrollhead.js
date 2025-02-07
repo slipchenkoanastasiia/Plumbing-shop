4e2b2558e82b9f4efd75bbffebb42045315ef5ef
slipchenkoanastasiia <138067087+slipchenkoanastasiia@users.noreply.github.com>
2025-02-07 12:32:16 +0200

    fix header and swipe

--- docs/js/scrollhead.js
+++ docs/js/scrollhead.js
@@ -1,9 +1,10 @@
-window.addEventListener('scroll', function () {
-    const header = document.querySelector('header');
-    if (window.scrollY > 50) {
-      header.classList.add('scrolled');
-    } else {
-      header.classList.remove('scrolled');
-    }
-  });
-  
+window.addEventListener("scroll", function () {
+  const header = document.querySelector(".header");
+
+  if (window.scrollY > 100) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
+
