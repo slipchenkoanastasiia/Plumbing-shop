b37a0bb7205ca4f13517701d894b5e3857aab46a
slipchenkoanastasiia <138067087+slipchenkoanastasiia@users.noreply.github.com>
2025-02-13 23:16:06 +0200

    add new sc

---
+++ dist/index.e293a4c4.js
@@ -0,0 +1,21 @@
+const sliderTrack = document.querySelector('.slider-track');
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
+let currentIndex = 0;
+const totalSlides = document.querySelectorAll('.product-card').length;
+nextBtn.addEventListener('click', ()=>{
+    if (currentIndex < totalSlides - 1) currentIndex++;
+    else currentIndex = 0;
+    updateSlider();
+});
+prevBtn.addEventListener('click', ()=>{
+    if (currentIndex > 0) currentIndex--;
+    else currentIndex = totalSlides - 1;
+    updateSlider();
+});
+function updateSlider() {
+    const slideWidth = document.querySelector('.product-card').offsetWidth;
+    sliderTrack.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
+}
+
+//# sourceMappingURL=index.e293a4c4.js.map
