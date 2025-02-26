a1aff3b4178f513fab2e4cb17d4a9b077ba87dfd
slipchenkoanastasiia <138067087+slipchenkoanastasiia@users.noreply.github.com>
2025-02-26 16:41:59 +0200

    fix sec

--- docs/js/scrollhead.js
+++ docs/js/scrollhead.js
@@ -1,10 +1,23 @@
 window.addEventListener("scroll", function () {
   const header = document.querySelector(".header");
+  const footer = document.querySelector(".footer");
+
+  if (!header || !footer) return;
+
+  const footerTop = footer.getBoundingClientRect().top;
+  const headerHeight = header.offsetHeight;
 
   if (window.scrollY > 100) {
-    header.classList.add("scrolled");
+      header.classList.add("scrolled");
   } else {
-    header.classList.remove("scrolled");
+      header.classList.remove("scrolled");
+  }
+
+  if (footerTop <= headerHeight + 10) { 
+      header.style.opacity = "0";
+      header.style.pointerEvents = "none";
+  } else {
+      header.style.opacity = "1";
+      header.style.pointerEvents = "auto";
   }
 });
-
