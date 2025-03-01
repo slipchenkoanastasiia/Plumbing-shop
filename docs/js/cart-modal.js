204c8238aa1145c3a911155d5ac2dddeb83b4c1c
slipchenkoanastasiia <138067087+slipchenkoanastasiia@users.noreply.github.com>
2025-03-01 18:03:36 +0200

    fix modals

--- docs/js/cart-modal.js
+++ docs/js/cart-modal.js
@@ -2,13 +2,11 @@ const cartIcon = document.getElementById('cart-icon');
 const cartModal = document.getElementById('cart-modal');
 const closeBtn = document.getElementById('close-btn');
 
-// Відкриваємо модалку при натисканні на іконку корзини
 cartIcon.addEventListener('click', (e) => {
-  e.preventDefault();  // Запобігаємо переходу
+  e.preventDefault(); 
   cartModal.style.display = 'block';
 });
 
-// Закриваємо модалку при натисканні на хрестик
 closeBtn.addEventListener('click', () => {
   cartModal.style.display = 'none';
 });
