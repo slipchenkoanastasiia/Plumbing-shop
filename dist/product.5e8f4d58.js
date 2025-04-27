3be0c0f075de4f62326f2997dcc6ed7ecca14965
slipchenkoanastasiia <138067087+slipchenkoanastasiia@users.noreply.github.com>
2025-04-27 23:28:43 +0300

    add div

---
+++ dist/product.5e8f4d58.js
@@ -0,0 +1,10 @@
+const addToCartButton = document.querySelector('.add-to-cartg');
+const cartModalNotification = document.querySelector('.cart-modal-notification');
+addToCartButton.addEventListener('click', ()=>{
+    cartModalNotification.style.display = 'block';
+    setTimeout(()=>{
+        cartModalNotification.style.display = 'none';
+    }, 3000);
+});
+
+//# sourceMappingURL=product.5e8f4d58.js.map
---
+++ docs/js/add-to-cartg.js
@@ -0,0 +1,10 @@
+const addToCartButton = document.querySelector('.add-to-cartg');
+const cartModalNotification = document.querySelector('.cart-modal-notification');
+
+addToCartButton.addEventListener('click', () => {
+  cartModalNotification.style.display = 'block';
+
+  setTimeout(() => {
+    cartModalNotification.style.display = 'none';
+  }, 3000); 
+});
